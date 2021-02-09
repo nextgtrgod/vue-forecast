a35e7a1cef680158f725eb7577b6a84d2f7bd6fb
nextgtrgod <KYakovlev7@gmail.com>
2021-02-09 20:45:44 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo 'Deploy started'
 yarn build
 git add dist --force
 git commit -m 'deploy'
-git subtree push --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist master`:gh-pages --force
 
 echo 'Deployed: \033[1;32;4mhttps://nextgtrgod.github.io/vue-forecast/\033[0m'
 echo ''
